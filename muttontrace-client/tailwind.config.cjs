4ed47820ce977afa424621ea83c1348c5740fa82
Qian Zehao <qianzehao123@gmail.com>
2023-12-19 17:56:53 +0000

    tailwindcss config file

---
+++ muttontrace-client/tailwind.config.cjs
@@ -0,0 +1,28 @@
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+module.exports = {
+  content: ["./src/**/*.{html,vue,js,ts}"],
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      "fantasy",
+      "wireframe",
+      "black",
+      "luxury",
+      "dracula",
+      "cmyk",
+      "autumn",
+      "business",
+      "acid",
+      "lemonade",
+      "night"
+    ],
+  },
+};
