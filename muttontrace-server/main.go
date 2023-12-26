09b84a4876a64caab11ccd5b2ccc0194931b728c
Qian Zehao <qianzehao123@gmail.com>
2023-12-26 22:15:37 +0000

    .

--- muttontrace-server/main.go
+++ muttontrace-server/main.go
@@ -3,75 +3,73 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"mutton/testFunc"
-	"net/http"
 )
 
-var db = make(map[string]string)
+type StructA struct {
+	FieldA string `form:"field_a"`
+}
 
-func setupRouter() *gin.Engine {
-	// Disable Console Color
-	// gin.DisableConsoleColor()
-	r := gin.Default()
+type StructB struct {
+	NestedStruct StructA
+	FieldB       string `form:"field_b"`
+}
 
-	// Ping test
-	r.GET("/ping", func(c *gin.Context) {
-		c.String(http.StatusOK, "pong")
+type StructC struct {
+	NestedStructPointer *StructA
+	FieldC              string `form:"field_c"`
+}
+
+type StructD struct {
+	NestedAnonyStruct struct {
+		FieldX string `form:"field_x"`
+	}
+	FieldD string `form:"field_d"`
+}
+
+func GetDataB(c *gin.Context) {
+	var b StructB
+	err := c.Bind(&b)
+	if err != nil {
+		return
+	}
+	c.JSON(200, gin.H{
+		"a": b.NestedStruct,
+		"b": b.FieldB,
 	})
+}
 
-	// Get user value
-	r.GET("/user/:name", func(c *gin.Context) {
-		user := c.Params.ByName("name")
-		value, ok := db[user]
-		if ok {
-			c.JSON(http.StatusOK, gin.H{"user": user, "value": value})
-		} else {
-			c.JSON(http.StatusOK, gin.H{"user": user, "status": "no value"})
-		}
+func GetDataC(c *gin.Context) {
+	var b StructC
+	err := c.Bind(&b)
+	if err != nil {
+		return
+	}
+	c.JSON(200, gin.H{
+		"a": b.NestedStructPointer,
+		"c": b.FieldC,
 	})
+}
 
-	// Authorized group (uses gin.BasicAuth() middleware)
-	// Same than:
-	// authorized := r.Group("/")
-	// authorized.Use(gin.BasicAuth(gin.Credentials{
-	//	  "foo":  "bar",
-	//	  "manu": "123",
-	//}))
-	authorized := r.Group("/", gin.BasicAuth(gin.Accounts{
-		"foo":  "bar", // user:foo password:bar
-		"manu": "123", // user:manu password:123
-	}))
-
-	/* example curl for /admin with basicauth header
-	   Zm9vOmJhcg== is base64("foo:bar")
-
-		curl -X POST \
-	  	http://localhost:8080/admin \
-	  	-H 'authorization: Basic Zm9vOmJhcg==' \
-	  	-H 'content-type: application/json' \
-	  	-d '{"value":"bar"}'
-	*/
-	authorized.POST("admin", func(c *gin.Context) {
-		user := c.MustGet(gin.AuthUserKey).(string)
-
-		// Parse JSON
-		var json struct {
-			Value string `json:"value" binding:"required"`
-		}
-
-		if c.Bind(&json) == nil {
-			db[user] = json.Value
-			c.JSON(http.StatusOK, gin.H{"status": "ok"})
-		}
+func GetDataD(c *gin.Context) {
+	var b StructD
+	err := c.Bind(&b)
+	if err != nil {
+		return
+	}
+	c.JSON(200, gin.H{
+		"x": b.NestedAnonyStruct,
+		"d": b.FieldD,
 	})
-
-	return r
 }
 
 func main() {
 	testFunc.OutputStr()
-	r := setupRouter()
-	// Listen and Server in 0.0.0.0:8080
-	err := r.Run(":8080")
+	r := gin.Default()
+	r.GET("/getb", GetDataB)
+	r.GET("/getc", GetDataC)
+	r.GET("/getd", GetDataD)
+
+	err := r.Run()
 	if err != nil {
 		return
 	}
